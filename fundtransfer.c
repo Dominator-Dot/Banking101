e9b77753515e166e8c16e909129f1f0247f22f16
Nhuja2735 <kaicha2735@gmail.com>
2025-02-09 21:58:53 +0545

    updated

--- fundtransfer.c
+++ fundtransfer.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
 
-void transferFunds(float *fromAccount, float *toAccount, float amount) {
-    if (*fromAccount >= amount) {
+void transferFunds(float *fromAccount, float *toAccount, float amount)
+{
+    if (*fromAccount >= amount)
+    {
         *fromAccount -= amount;
         *toAccount += amount;
         printf("Transfer successful. Transferred %.2f\n", amount);
-    } else {
+    }
+    else
+    {
         printf("Insufficient funds. Transfer failed.\n");
     }
 }
 
-int main() {
+int main()
+{
     float account1 = 1000.0;
     float account2 = 500.0;
-    float amount = 200.0;
+    float amount;
+
+    printf("Enter the amount to transfer: ");
+    scanf("%f", &amount);
 
     printf("Account 1 balance: %.2f\n", account1);
     printf("Account 2 balance: %.2f\n", account2);
